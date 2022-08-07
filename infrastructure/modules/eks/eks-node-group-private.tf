0df6e2fa4f4662cb099eaa28e4b649c8da9692d1
Ty <tycloud97@gmail.com>
2022-08-07 14:56:45 +0700

    feat: change to medium

--- infrastructure/modules/eks/eks-node-group-private.tf
+++ infrastructure/modules/eks/eks-node-group-private.tf
@@ -10,7 +10,7 @@ resource "aws_eks_node_group" "eks_ng_private" {
   ami_type       = "AL2_x86_64"
   capacity_type  = "ON_DEMAND"
   disk_size      = 20
-  instance_types = ["t3.large"]
+  instance_types = ["t3.medium"]
 
 
   remote_access {
